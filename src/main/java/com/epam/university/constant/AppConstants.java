ed2482e5b35567e06422568b260979a7e843405e
Valeriia Fatova <valeriia.fatova@gmail.com>
2019-10-14 00:31:50 +0300

    05 add jdbc connection

---
+++ src/main/java/com/epam/university/constant/AppConstants.java
@@ -0,0 +1,13 @@
+package com.epam.university.constant;
+
+public class AppConstants {
+    
+    public static final String DB_PROPERTIES = "/db.properties";
+    public static final String DB_URL = "DB_URL";
+    public static final String DB_NAME = "DB_NAME";
+    public static final String DB_CHARACTER_ENCODING = "DB_CHARACTER_ENCODING";
+    public static final String DB_USERNAME = "DB_USERNAME";
+    public static final String DB_PASSWORD = "DB_PASSWORD";
+    
+    
+}

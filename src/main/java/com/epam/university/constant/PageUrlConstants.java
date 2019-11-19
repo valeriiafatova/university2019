dd8ea080ce6e466ba7072bdc2eb96edf48757950
Valeriia Fatova <valeriia.fatova@gmail.com>
2019-11-19 23:36:34 +0200

    15-01 Add authentication filter and 403 command

--- src/main/java/com/epam/university/constant/PageUrlConstants.java
+++ src/main/java/com/epam/university/constant/PageUrlConstants.java
@@ -3,9 +3,9 @@ package com.epam.university.constant;
 public class PageUrlConstants {
 
     public static final String HOME_PAGE = "index";
-    public static final String USER_PAGE = "user";
     public static final String REGISTER_PAGE = "registration";
-    public static final String NOT_FOUND_PAGE = "404";
+    public static final String NOT_FOUND_PAGE = "error/404-error";
+    public static final String FORBIDDEN_PAGE = "errors/403-error";
     public static final String ABOUT_US_PAGE = "about-us";
     public static final String BLOG_PAGE = "blog";
     public static final String BLOG_DETAIL_PAGE = "single-blog";
